d38a152dd2f2a25fe29c85e036a2c198cabd467a
Miyoung <rhaldud89@gmail.com>
2021-11-23 18:09:40 +0900

    Initial commit

---
+++ src/main/java/com/gom/login/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.gom.login.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public void test(){
+        System.out.println("#####");
+
+    }
+
+
+}
